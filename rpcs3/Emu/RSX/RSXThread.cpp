f933b305daa848b7889d8dbec668f77a817cf908
Rajko Stojadinovic <admin@rajko.info>
2019-10-02 20:15:48 +0200

    use "ZCull query default value" video setting as default output value when ZCull occulsion queries are disabled

--- rpcs3/Emu/RSX/RSXThread.cpp
+++ rpcs3/Emu/RSX/RSXThread.cpp
@@ -2359,6 +2359,8 @@ namespace rsx
 				break;
 			}
 		}
+		else
+			value = g_cfg.video.zcull_default_value;
 
 		rsx::reservation_lock<true> lock(sink, 16);
 		vm::_ref<atomic_t<CellGcmReportData>>(sink).store({ timestamp(), value, 0});
