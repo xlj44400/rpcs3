5765f8473ef476debd0c2bbc635732b47ce9c308
Rajko Stojadinovic <admin@rajko.info>
2019-10-02 20:15:48 +0200

    use "ZCull query default value" video setting as default output value when ZCull occulsion queries are disabled

--- rpcs3/Emu/RSX/RSXThread.cpp
+++ rpcs3/Emu/RSX/RSXThread.cpp
@@ -2396,6 +2396,8 @@ namespace rsx
 				break;
 			}
 		}
+		else
+			value = g_cfg.video.zcull_default_value;
 
 		vm::_ref<atomic_t<CellGcmReportData>>(sink).store({ timestamp(), value, 0});
 	}
--- rpcs3/Emu/system_config.h
+++ rpcs3/Emu/system_config.h
@@ -156,6 +156,7 @@ struct cfg_root : cfg::node
 		cfg::_bool strict_texture_flushing{ this, "Strict Texture Flushing", false };
 		cfg::_bool disable_native_float16{ this, "Disable native float16 support", false };
 		cfg::_bool multithreaded_rsx{ this, "Multithreaded RSX", false };
+		cfg::_int<0, 0xFFFFFFFFu> zcull_default_value{this, "ZCull query default value", 1};
 		cfg::_bool relaxed_zcull_sync{ this, "Relaxed ZCULL Sync", false };
 		cfg::_bool enable_3d{ this, "Enable 3D", false };
 		cfg::_bool debug_program_analyser{ this, "Debug Program Analyser", false };
