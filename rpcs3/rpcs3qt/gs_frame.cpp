f62691d9a41cf748a57439e9305b0129aaa06a96
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2020-10-02 04:49:20 -0400

    Remove "Fake input" Commit
    This reverts commit 04681f4e320f2946506778c9e37c6ee22bdad025.

--- rpcs3/rpcs3qt/gs_frame.cpp
+++ rpcs3/rpcs3qt/gs_frame.cpp
@@ -136,8 +136,6 @@ void gs_frame::showEvent(QShowEvent *event)
 	QWindow::showEvent(event);
 }
 
-bool fakeinput_toggle = false;
-
 void gs_frame::keyPressEvent(QKeyEvent *keyEvent)
 {
 	// NOTE: needs to be updated with keyboard_pad_handler::processKeyEvent
@@ -216,9 +214,6 @@ void gs_frame::keyPressEvent(QKeyEvent *keyEvent)
 		break;
 	case Qt::Key_F12:
 		screenshot_toggle = true;
-		break;
-	case Qt::Key_F:
-		fakeinput_toggle = !fakeinput_toggle;
 		break;
 	default:
 		break;
